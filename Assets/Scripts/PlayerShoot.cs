e06e4698b949c5d01f8957575255d3a7e0df97a5
Tristan <ricemaking69@gmail.com>
2024-03-23 11:34:37 -0500

    Player shooting bullets is now smooth

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -12,10 +12,15 @@ public class PlayerShoot : MonoBehaviour
     public float bulletSpeed = 12f;
 
     // The jamming chance.
-    public float jamChance = 0.05f;
+    public float jamChance = 0.1f;
 
     // The jam cooldown.
-    public float jamCooldownDuration = 0.10f;
+    // The minimum and maximum jam cooldown durations.
+    public float minJamCooldownDuration = 0.5f;
+    public float maxJamCooldownDuration = 0.9f;
+
+    // Time between shots.
+    public float timeBetweenShots = 0.05f;
 
     // Offset the player's position to spawn the bullet.
     public Vector3 bulletSpawnOffset = new Vector3(0f, 0.5f, 0f);
@@ -25,6 +30,9 @@ public class PlayerShoot : MonoBehaviour
 
     // Checks if it is jammed.
     private bool isJammed = false;
+
+    // Private for the cooldown variable for jamming.
+    private float jamCooldownDuration;
 
     // Update is called once per frame
     void Update()
@@ -36,13 +44,14 @@ public class PlayerShoot : MonoBehaviour
         }
 
         // Shooting with right mouse button
-        else if (Input.GetKey(KeyCode.Mouse0) && !isJammed)
+        else if (Input.GetKey(KeyCode.Mouse0) && !isJammed && Time.time - lastShotTime >= timeBetweenShots)
         {
             // Check if the gun jams
             if (Random.value < jamChance)
             {
                 isJammed = true;
                 lastShotTime = Time.time;
+                jamCooldownDuration = Random.Range(minJamCooldownDuration, maxJamCooldownDuration);
                 Debug.Log("Gun jammed!");
                 return; 
             }
