f1c9df7d6190ed8136bf317835d9e909ad99d6bb
Tristan <ricemaking69@gmail.com>
2024-03-22 23:27:06 -0500

    Update PlayerShoot..cs

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public float jamChance = 0.25f;
+    // The jamming chance.
+    public float jamChance = 0.05f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // The jam cooldown.
+    public float jamCooldownDuration = 0.10f;
+
+    // The last shot time.
+    private float lastShotTime = 0f;
+
+    // Checks if it is jammed.
+    private bool isJammed = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        // Timer if statement
+        if (isJammed && Time.time - lastShotTime >= jamCooldownDuration)
+        {
+            isJammed = false; 
+        }
+
+        // Shooting with right mouse button
+        else if (Input.GetKey(KeyCode.Mouse0) && !isJammed)
         {
             // Check if the gun jams
             if (Random.value < jamChance)
             {
-                // Probaly do an animation
+                isJammed = true;
+                lastShotTime = Time.time;
                 Debug.Log("Gun jammed!");
-                return; 
+                return; // Exit the Update method if the gun jams
             }
 
-            
+            // If the gun doesn't jam, shoot normally
             Debug.Log("Shoot!");
+            lastShotTime = Time.time;
         }
     }
 }
