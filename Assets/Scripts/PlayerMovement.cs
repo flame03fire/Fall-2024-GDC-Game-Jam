3e7720aca5b9d76f24e259a6af9e89adf7c282c4
Tristan <ricemaking69@gmail.com>
2024-03-23 17:02:44 -0500

    Aimming is more accurate

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,6 @@ public class PlayerMovement : Moveable
         direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         // Update the body's direction.
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 }
