a601cc76cdae7038097826ed517d2a3e14e5973c
Tristan <ricemaking69@gmail.com>
2024-03-23 12:26:52 -0500

    Bullet collision.
    Now bullets should die after colliding with another game object.

---
+++ Assets/Scripts/BulletCollide.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollide : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object is not a bullet
+        if (!collision.gameObject.CompareTag("Bullet"))
+        {
+            // Destroy the bullet
+            Destroy(gameObject);
+        }
+    }
+}
