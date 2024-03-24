c1c6f7131e343ea1409acc0b8612e0c3c751bd28
Tristan <ricemaking69@gmail.com>
2024-03-24 15:34:40 -0500

    Game over screen here.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : Moveable
 {
@@ -33,6 +34,7 @@ public class PlayerMovement : Moveable
         if (collision.gameObject.CompareTag("Zombie"))
         {
             Destroy(gameObject);
+            SceneManager.LoadSceneAsync(2);
         }
     }
 }
